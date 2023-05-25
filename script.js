ac04af18426d77021afd71fb82cfcbd45be9b8c5
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-05-25 16:52:43 +0100

    add the first else if statement

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var score = prompt("Input your score");
 
 if(score >= 0 && score <= 30)
-document.write("Try Harder your score is low");
+document.write("Try Harder your score is low!");
+
+else if(score >=31 && score <= 60)
+document.write("Hey that is a good score but you can do better!");
 
 
