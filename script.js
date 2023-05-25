766f1125a4c45022bd91053adf902a7d2a597720
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-05-25 16:55:13 +0100

    add second else if statement

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var score = prompt("Input your score");
 if(score >= 0 && score <= 30)
 document.write("Try Harder your score is low!");
 
-else if(score >=31 && score <= 60)
+else if(score >= 31 && score <= 60)
 document.write("Hey that is a good score but you can do better!");
 
+else if(score >= 61 && score <= 100)
+document.write("Wow you did great!");
 
